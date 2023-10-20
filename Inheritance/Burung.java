097b66f811657f3aa161d7cb337b295e263ccb01
mah11labib <145730187+mah11labib@users.noreply.github.com>
2023-10-20 08:48:42 +0700

    Add files via upload

---
+++ Inheritance/Burung.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Inheritance;
+
+/**
+ *
+ * @author pc20
+ */
+    public class Burung extends Enemy{
+        void walk(){
+        System.out.println("Burung berjalan");
+    }
+    void jump(){
+        System.out.println("Burung loncat loncat");
+    }
+    void fly(){
+        System.out.println("Burung Terbang");
+    }
+
+}
+
